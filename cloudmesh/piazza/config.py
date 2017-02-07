1d3c33a7bcaefb4a29f784eb80a7e16e1f28c025
Tim Whitson <whitstd@gmail.com>
2017-02-07 17:35:52 -0500

    add version command

--- cloudmesh/piazza/config.py
+++ cloudmesh/piazza/config.py
@@ -10,3 +10,11 @@ def encrypt(string):
 
 def decrypt(string):
     return base64.b64decode(string)
+    
+def change(section, item, value):
+    config = ConfigParser.SafeConfigParser()
+    config.readfp(open(os.path.expanduser('~/piazza/piazza.cfg')))
+    config.set(section, item, value)
+    
+    with open(os.path.expanduser('~/piazza/piazza.cfg'), 'wb') as f:
+        config.write(f)
